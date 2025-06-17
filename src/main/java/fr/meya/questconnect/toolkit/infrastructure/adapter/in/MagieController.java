901a9870854e544a2e7f54bea4ec68407e0c698c
Mélody Delage <angie-island@hotmail.fr>
2025-06-17 16:34:43 +0200

    Branchement back and front Magie

--- src/main/java/fr/meya/questconnect/toolkit/infrastructure/adapter/in/MagieController.java
+++ src/main/java/fr/meya/questconnect/toolkit/infrastructure/adapter/in/MagieController.java
@@ -2,6 +2,7 @@ package fr.meya.questconnect.toolkit.infrastructure.adapter.in;
 
 import fr.meya.questconnect.toolkit.service.MagieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class MagieController {
     @Autowired
     private MagieService magieService;
 
-    @RequestMapping
+    @GetMapping
     public String getMagieList(){
         return magieService.getMagieList();
     }
