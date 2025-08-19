bce56cb88b83795064c3edf240558646f1fbd608
Mélody Delage <angie-island@hotmail.fr>
2025-08-19 12:17:47 +0200

    maj profesion update

--- src/main/java/fr/meya/questconnect/toolkit/infrastructure/adapter/in/ProfessionController.java
+++ src/main/java/fr/meya/questconnect/toolkit/infrastructure/adapter/in/ProfessionController.java
@@ -1,12 +1,14 @@
 package fr.meya.questconnect.toolkit.infrastructure.adapter.in;
 
 import fr.meya.questconnect.toolkit.service.ProfessionService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/professions")
 public class ProfessionController {
@@ -15,12 +17,20 @@ public class ProfessionController {
 
     @GetMapping
     public String getProfessionList(){
-        return professionService.getProfessionList();
+        log.info("Récupération de la liste des professions");
+        String response = professionService.getProfessionList();
+        log.info("Liste des professions récupérée avec succès");
+
+        return response;
     }
 
     @GetMapping("/{id}/competences")
     public String getProfessionCompetences(@PathVariable Long id){
-        return professionService.getProfessionCompetences(id);
+        log.info("Récupération de la liste compétences d'une profession");
+        String response =  professionService.getProfessionCompetences(id);
+        log.info("Liste compétences d'une profession récupérée avec succès");
+
+        return response;
     }
 
 }
--- src/main/java/fr/meya/questconnect/toolkit/infrastructure/adapter/out/ProfessionAdapter.java
+++ src/main/java/fr/meya/questconnect/toolkit/infrastructure/adapter/out/ProfessionAdapter.java
@@ -1,26 +1,38 @@
 package fr.meya.questconnect.toolkit.infrastructure.adapter.out;
 
 import fr.meya.questconnect.toolkit.port.out.IProfessionAdapter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Component
 public class ProfessionAdapter implements IProfessionAdapter {
-    @Value("${toolkit.api.base.url}")
+    @Value("${toolkit.api.base.url}/profession")
     private String baseUrl;
 
     @Override
     public String getProfessionList() {
         RestTemplate restTemplate = new RestTemplate();
-        String url = baseUrl + "/profession/list";
-        return restTemplate.getForObject(url, String.class);
+        String url = baseUrl + "/list";
+
+        log.info("Adapter - Envoi de la requête GET vers {}", url);
+        String response = restTemplate.getForObject(url, String.class);
+        log.info("Adapter getList - Réponse reçue : {}", response);
+
+        return response;
     }
 
     @Override
     public String getProfessionCompetences(Long id) {
         RestTemplate restTemplate = new RestTemplate();
-        String url = baseUrl + "/profession/detail/" + id;
-        return restTemplate.getForObject(url, String.class);
+        String url = baseUrl + "/detail/" + id;
+
+        log.info("Adapter - Envoi de la requête GET getProfessionCompetence vers {}", url);
+        String response = restTemplate.getForObject(url, String.class);
+        log.info("Adapter getProfessionCompetence - Réponse reçue : {}", response);
+
+        return response;
     }
 }
